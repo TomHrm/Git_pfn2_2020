3db90fcfe30519363ad3d3efac1c73615b5fce95
Dominick_Laptop <d.jamesjux@gmail.com>
2020-04-30 09:57:26 +0200

    Initialisierung Blatt 01

---
+++ Blatt01/Aufgabe2/LimitsTsv/limits_tsv.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+	
+	char, short, int, long, 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	if (argc != 1)
+	{
+		for(int i=1; i<argc; i++)
+		{
+			printf("Hello %s\n",argv[i]);
+		}
+	}
+	else
+	{
+	printf("Hello World\n");
+	}
+	return 0;
+	*/
+}
