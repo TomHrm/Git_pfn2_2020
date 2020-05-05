a7dd2d5f2fef186b54abf7edc7613a469b64d9a7
Tom Herrmann <tom_herrmannhhoutlook.de>
2020-05-05 18:27:47 +0200

    Aufgabe 1 Done

--- Blatt02/Aufgabe1/FacultyDebug/faculty.c
+++ Blatt02/Aufgabe1/FacultyDebug/faculty.c
@@ -9,12 +9,12 @@ int main(int argc, char *argv[])
   if (argc != 2 || sscanf(argv[1],"%d",&n) != 1 || n < 0)
   {
     fprintf(stderr," Usage: {} <positive_integer %s>\t\n" , argv[1]);
-    return 1;
+    return EXIT_FAILURE;
   }
   for (int count = 1; count <= n; count++)
     {
       value = value * count;
     }
   printf("%d\t%ld\n",n,value);
-  return 0;
+  return EXIT_SUCCESS;
 }
