f556fe9dd5a933465f8e5ef973441b77643f6536
Tom Herrmann <tom_herrmannhhoutlook.de>
2020-05-07 11:53:48 +0200

    Blatt 2 done

--- Blatt02.Herrmann.Labatz.Noack/Aufgabe2/Ascii/ascii.c
+++ Blatt02.Herrmann.Labatz.Noack/Aufgabe2/Ascii/ascii.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 
-int main(int argc, char *argv[])
+int main()
 {
+	int n;
+	char dict[] = {'a', 'b', 't', 'n', 'v', 'f', 'r'};
+		for(char r = 0; r < 32; r++)
+		{
+			for(char i = 0; i < 4;i++)
+			{
+				n = r + i * 32;
+				if(n >= 33 && n <= 126)
+				{
+					printf("%3d%5c", n, n); 
+					//führende "0" werden mit leerzeihen gefüllt
+				}
+				else if(n > 6 && n < 14)
+				{
+					printf("%3d   \\%c",n , dict[n-7]);
+				}
+				else
+				{
+					printf("%3d%s\\%d", n,  n < 10 ? "   ": ( n < 100 ? "  ": " "), n);
+				}
+				if(i < 3)
+				{
+					printf("\t");
+				}
+			}
+		printf("\n");
+		}
 
-  return EXIT_SUCCESS;
+  	return EXIT_SUCCESS;
 }
