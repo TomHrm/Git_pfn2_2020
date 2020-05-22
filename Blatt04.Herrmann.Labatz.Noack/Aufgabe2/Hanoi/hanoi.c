0ef2b5b50464cbc6129dd3f310a06af5dd18dbc7
Herrmann <46518958+TomHrm@users.noreply.github.com>
2020-05-22 11:46:39 +0200

    Update hanoi.c

--- Blatt04.Herrmann.Labatz.Noack/Aufgabe2/Hanoi/hanoi.c
+++ Blatt04.Herrmann.Labatz.Noack/Aufgabe2/Hanoi/hanoi.c
@@ -1,37 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-void hanoi(int size, int start, int goal)
+int hanoimoves(n)
 {
-    if (size > 1)
-    {
-        hanoi(size - 1, start, 6 - start - goal);
-        printf("(%d,%d) ", start, goal);
-        hanoi(size - 1, 6 - start - goal, goal);
-
-    } else {
-        printf("(%d,%d) ", start, goal);
-    }
+    
 }
-
 
 int main(int argc, char const *argv[])
 {
     if (argc != 2) {
-        fprintf(stderr, "%s: Invalid number of arguments.\n", argv[0]);
+        fprintf(stderr, "%s: falsche Argumentennummer.\n", argv[0]);
         return EXIT_FAILURE;
     }
 
     int n;
     if (sscanf(argv[1], "%d", &n) != 1 || n <= 0) {
-        fprintf(stderr, "%s: Input is not a positive integer.\n", argv[0]);
+        fprintf(stderr, "%s: nicht positiv.\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    hanoi(n, 1, 2);
-
-    printf("\n");
     
     return EXIT_SUCCESS;
 }
