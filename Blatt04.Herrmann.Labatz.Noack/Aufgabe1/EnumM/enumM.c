6f1df6194fd0d6a529408ec93545a21c73321f98
Herrmann <46518958+TomHrm@users.noreply.github.com>
2020-05-23 12:37:49 +0200

    Update enumM.c

---
+++ Blatt04.Herrmann.Labatz.Noack/Aufgabe1/EnumM/enumM.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>	
+#include <stdio.h>	
+#include <stdbool.h>	
+
+int main(int argc, char *argv[])	
+{	
+  unsigned long n; //die Obergrenze der Menge M	
+  unsigned long steps = 0; //die Anzahl der Schritte	
+  if(argc != 2 || (	sscanf(argv[1], "%lu", &n)) != 1)	
+  {	
+  fprintf(stderr, "Ungültiges Argument");	
+  exit(EXIT_FAILURE);	
+  }	
+  char *arr = calloc(n, sizeof *arr);	
+  //Char-Array mit Kapazität, in dem '1' Index 	
+  //eines in M enthaltenen Elementes steht, sonst '0'	
+
+  int i = 1; //Laufvariable	
+  arr[i] = 1;	
+
+  steps += 4; //zuweisungen	
+  steps += 3; //Funktionsaufrufe	
+
+  //bis hier eh O(1)...	
+
+
+  while(i <= n)	
+    {	
+      steps++; //vergleich	
+
+      steps += 2; //if, Indexzugriff	
+      if(arr[i] == 1) 	
+      {	
+      	steps += 3; // Arithmetik, Test	
+        printf("%d\n", i);	
+        if((2*i+1) <= n)	
+        {	
+	          arr[2*i+1] = 1;	
+	          steps += 4; //Wertzuweisung, Indexzugriff, Arithmetik	
+        } 	
+
+        steps += 3; // Arithmetik ,Test	
+        if((3*i+1) <= n)	
+        {	
+          arr[3*i+1] = 1;	
+          steps += 4; // Arithmetik,Indexzugriff, wertzuweisung	
+        }	
+
+      }	
+
+    steps++; //Wertzuweisung	
+    i++;	
+
+  } //n Steps für n Schleifendurchläufe	
+  steps += n; 	
+
+
+  steps++; //Freigabe von Speicherplatz	
+  free(arr); 	
+
+  printf("#%lu\t%lu\n", n, steps);	
+
+return EXIT_SUCCESS;	
+}
