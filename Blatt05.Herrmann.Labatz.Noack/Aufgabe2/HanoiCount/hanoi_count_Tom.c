3928b62ae4bb868878cc4a87de344f62270d78b5
Herrmann, Tom <tom.herrmann@studium.uni-hamburg.de>
2020-05-29 13:18:10 +0200

    Issue #5 - Done

--- Blatt05.Herrmann.Labatz.Noack/Aufgabe2/HanoiCount/hanoi_count_Tom.c
+++ Blatt05.Herrmann.Labatz.Noack/Aufgabe2/HanoiCount/hanoi_count_Tom.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int hanoi_rec_count(int num_of_disks, int from_pole, int to_pole, int count)
+static int hanoi_rec_count(int num_of_disks, int from_pole, int to_pole)
 {
   if (num_of_disks > 0)
   {
@@ -14,23 +14,21 @@ static int hanoi_rec_count(int num_of_disks, int from_pole, int to_pole, int cou
        Then we can move the largest disk to the to_pole, and after that we
        move the stack from tmp_pole to the to_pole. */
 
-    	int tmp_pole = 6 - from_pole - to_pole;
-	count+=1;
-  return (1+ hanoi_rec_count(num_of_disks - 1, from_pole, tmp_pole,count) 
-           + hanoi_rec_count(num_of_disks - 1, tmp_pole, to_pole,count));
+    int tmp_pole = 6 - from_pole - to_pole;
+    return 1 + (2 * hanoi_rec_count(num_of_disks - 1, from_pole, tmp_pole));
   }
   else
   {
-  return 0;
+  	return 0;
   }
 }
 
-static void hanoimoves(int num_of_disks)
+static int hanoimoves(int num_of_disks)
 {
   const int from_pole = 1,
             to_pole = 2;
 
-  printf("%d\t%d\n", num_of_disks, hanoi_rec_count(num_of_disks, from_pole, to_pole,0));
+  return hanoi_rec_count(num_of_disks, from_pole, to_pole);
 }
 
 int main(int argc, char *argv[])
@@ -43,9 +41,10 @@ int main(int argc, char *argv[])
     fprintf(stderr,"Usage: %s <positive integer>\n", argv[0]);
     return EXIT_FAILURE;
   }
-  for (int i = 1 ; i <= num_of_disks ; i++)
+
+  for(int i = 1; i <= num_of_disks; i++)
   {
-    hanoimoves(i);
+  	printf("%d\t%d\n",i , hanoimoves(i));
   }
   return EXIT_SUCCESS;
 }
