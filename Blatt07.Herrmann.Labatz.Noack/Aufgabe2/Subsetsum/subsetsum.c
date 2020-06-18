bdfead38cbdf68e1b7f1cfaf8501312790cf30e9
Herrmann, Tom <tom.herrmann@studium.uni-hamburg.de>
2020-06-18 14:48:02 +0200

    Aufgabe 2 init

---
+++ Blatt07.Herrmann.Labatz.Noack/Aufgabe2/Subsetsum/subsetsum.c
@@ -0,0 +1,20 @@
+#include "subsetsum.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include "array2dim.h"
+#include <stdio.h>
+
+bool *subsetsum(const unsigned long *arr,
+                unsigned long r,
+                unsigned long s);
+{
+    unsigned long i;
+    
+}
+
+bool *subsetsum_memo(const unsigned long *arr,
+                     unsigned long r,
+                     unsigned long s);
+{
+
+}
